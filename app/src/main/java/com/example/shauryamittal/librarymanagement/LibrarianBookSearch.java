cbf51b1186df63c20fbd7232b9e0a51b6b07b6a6
Harshit Agarwal <Harshit@harshit.local>
2017-12-04 22:17:00 -0800

    harry

--- app/src/main/java/com/example/shauryamittal/librarymanagement/LibrarianBookSearch.java
+++ app/src/main/java/com/example/shauryamittal/librarymanagement/LibrarianBookSearch.java
@@ -3,13 +3,11 @@ package com.example.shauryamittal.librarymanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LibrarianBookSearch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("Main Activity---------------");
-
+        setContentView(R.layout.activity_librarian_book_search);
     }
 }
