ccc47c411181b0c8b29fa966cca16a2f8c397e52
shauryam <shaurya_mittal@ymail.com>
2017-12-04 21:00:57 -0800

    "Style" tag removed which was causing error

--- app/src/main/java/com/example/shauryamittal/librarymanagement/SignupActivity.java
+++ app/src/main/java/com/example/shauryamittal/librarymanagement/SignupActivity.java
@@ -2,13 +2,34 @@ package com.example.shauryamittal.librarymanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText sjsuId;
+    EditText email;
+    EditText password;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        sjsuId = (EditText) findViewById(R.id.sjsu_id);
+        email = (EditText) findViewById(R.id.email_signup);
+        password = (EditText) findViewById(R.id.signup_password);
+
+        signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
