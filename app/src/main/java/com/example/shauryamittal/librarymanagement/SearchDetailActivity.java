3750de4fb65dde2050010b10415e8547eb6d0ad3
Anshul Agrawal <anshul.dec7@gmail.com>
2017-12-06 01:48:46 -0800

    Added Add to card with sharedPreferences

--- app/src/main/java/com/example/shauryamittal/librarymanagement/SearchDetailActivity.java
+++ app/src/main/java/com/example/shauryamittal/librarymanagement/SearchDetailActivity.java
@@ -1,12 +1,22 @@
 package com.example.shauryamittal.librarymanagement;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SearchDetailActivity extends AppCompatActivity {
 
@@ -18,7 +28,7 @@ public class SearchDetailActivity extends AppCompatActivity {
     private TextView bookAuthor;
     private TextView bookCopies;
     private DatabaseReference mDatabase;
-    private int bookId ;
+    private String bookId ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +42,42 @@ public class SearchDetailActivity extends AppCompatActivity {
         bookCallNum=findViewById(R.id.book_call);
         Intent intent = getIntent();
         //bookId = Integer.parseInt(intent.getStringExtra("bookId"));
-        bookId = 4;
-        FirebaseDatabase database=FirebaseDatabase.getInstance();
-        DatabaseReference mRef=database.getReference().child("books");
+        bookId = "4dsLA3qXZFk8EMhNEFzn";
+        FirebaseFirestore database= FirebaseFirestore.getInstance();
+        DocumentReference mRef=database.collection("books").document(bookId);
+        mRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    bookAuthor.setText(doc.getString("author"));
+                    bookCallNum.setText(doc.getString("callNumber"));
+                    bookTitle.setText(doc.getString("title"));
+                    bookYear.setText(doc.getString("yearOfPub"));
+                    bookPublisher.setText(doc.getString("publisher"));
+                    bookStatus.setText(doc.getString("status"));
 
+                }
+            }
+        });
 
+    }
+
+    public void addToCart(View view){
+        SharedPreferences SP;
+        SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String bookId_sp = SP.getString("bookId", null);
+        SharedPreferences.Editor edit = null;
+        if (bookId_sp == null){
+        edit.putString("bookId", bookId);
+        }
+        else{
+            StringBuilder sb = new StringBuilder(bookId_sp);
+            sb.append(",").append(bookId);
+            edit.putString("bookId", sb.toString());
+        }
+        edit.commit();
+        Log.d("Test","Book Id: " + SP.getString("bookId", null));
 
     }
 }
