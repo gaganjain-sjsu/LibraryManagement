f132cb09919acd76d2776f117819229ce099fede
Harshit Agarwal <agarwalharshit350@gmail.com>
2017-12-07 03:47:01 -0800

    adding patron search

---
+++ app/src/main/java/com/example/shauryamittal/librarymanagement/PatronBookSearch.java
@@ -0,0 +1,157 @@
+package com.example.shauryamittal.librarymanagement;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.example.shauryamittal.librarymanagement.model.Book;
+import com.example.shauryamittal.librarymanagement.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatronBookSearch extends AppCompatActivity {
+    Spinner patronSpinner;
+    RecyclerView recyclerView;
+    PatronSearchAdapter psAdapter;
+    List<Book> books = new ArrayList<>();
+    private final String UID_KEY = "uid";
+    private final String FULLNAME_KEY = "fullname";
+    private final String EMAIL_KEY = "email";
+    private final String SJSU_ID_KEY = "sjsuId";
+    private final String ROLE_KEY = "role";
+    List<String> spinnerList = new ArrayList<String>();
+    List<String> spinnerKey = new ArrayList<String>();
+    String searchKey="";
+    private EditText searchKeywork;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patron_book_search);
+        patronSpinner = (Spinner) findViewById(R.id.patronSpinner);
+        spinnerList.add("Select Librarian");
+        spinnerKey.add("0");
+        getLibrarians();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, spinnerList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        patronSpinner.setAdapter(dataAdapter);
+
+    }
+
+
+    public void searchResult(View view){
+        searchKeywork=(EditText)findViewById(R.id.patronSearchtxt);
+        searchKey=searchKeywork.getText().toString();
+        Spinner dropDown=(Spinner)findViewById(R.id.patronSpinner);
+        String librarianId=spinnerKey.get(dropDown.getSelectedItemPosition());
+        books.clear();
+        recyclerView=(RecyclerView)findViewById(R.id.patronSearchRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        psAdapter= new PatronSearchAdapter(this,books);
+        recyclerView.setAdapter(psAdapter);
+        getSearchedBooks(librarianId);
+
+    }
+
+    public void getLibrarians() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .whereEqualTo(ROLE_KEY, "librarian")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                User librarian = new User(document.getString(FULLNAME_KEY),
+                                        document.getString(EMAIL_KEY),
+                                        document.getString(SJSU_ID_KEY),
+                                        document.getString(UID_KEY),
+                                        document.getString(ROLE_KEY));
+                                spinnerList.add(librarian.getName());
+                                spinnerKey.add(librarian.getUid());
+
+                            }
+                        } else {
+                            //Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+    public void getSearchedBooks(String librarianId) {
+        System.out.println("librarianId==="+librarianId);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        if(!librarianId.equals("0")){
+            db.collection("books").whereEqualTo("librarianId", librarianId)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+
+                                for (DocumentSnapshot document : task.getResult()) {
+                                    Book b1= new Book();
+                                    if(searchKey!=null && !searchKey.equals("")){
+                                        String keyWord=document.getString("keywords");
+                                        if(keyWord!=null && searchKey!=null && keyWord.toLowerCase().contains(searchKey.toLowerCase())){
+                                            b1 = document.toObject(Book.class);
+                                            b1.setBookId(document.getId());
+                                            books.add(b1);
+                                            psAdapter.notifyDataSetChanged();
+                                        }
+                                    }else{
+                                        b1 = document.toObject(Book.class);
+                                        b1.setBookId(document.getId());
+                                        books.add(b1);
+                                    }
+                                    psAdapter.notifyDataSetChanged();
+                                }
+
+
+                            } else {
+                               // Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+
+                        }
+                    });
+
+        }else{
+            db.collection("books")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (DocumentSnapshot document : task.getResult()) {
+                                    Book b1= new Book();
+                                    b1 = document.toObject(Book.class);
+                                    b1.setBookId(document.getId());
+                                    books.add(b1);
+                                    psAdapter.notifyDataSetChanged();
+                                }
+                            } else {
+                                //Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+
+                        }
+                    });
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/shauryamittal/librarymanagement/PatronSearchAdapter.java
@@ -0,0 +1,108 @@
+package com.example.shauryamittal.librarymanagement;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.shauryamittal.librarymanagement.model.Book;
+import com.example.shauryamittal.librarymanagement.model.DbOperations;
+
+import java.util.List;
+
+/**
+ * Created by Harshit on 12/5/17.
+ */
+
+public class PatronSearchAdapter extends RecyclerView.Adapter<PatronSearchAdapter.PatronSearchViewHolder>  {
+
+
+    private Context ctx;
+    private List<Book> mBookList;
+    private String currentBookId="";
+    private int currPosition;
+
+    public PatronSearchAdapter(Context ctx, List<Book> bookList) {
+        this.ctx = ctx;
+        mBookList = bookList;
+    }
+
+    @Override
+    public PatronSearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View view = inflater.inflate(R.layout.patron_search_layout,null);
+        return new PatronSearchViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PatronSearchViewHolder holder, int position) {
+
+        holder.bind(mBookList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBookList.size();
+    }
+
+    class PatronSearchViewHolder extends RecyclerView.ViewHolder{
+        Book mBook;
+        ImageView coverImage;
+        TextView title,author,yearOfPub;
+        //Button delete, update;
+        public PatronSearchViewHolder(View itemView) {
+            super(itemView);
+            coverImage=itemView.findViewById(R.id.PatronBookCoverImage);
+            title=itemView.findViewById(R.id.PatronTitle);
+            author=itemView.findViewById(R.id.PatronAuthor);
+            yearOfPub=itemView.findViewById(R.id.PatronYearOfPub);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(ctx, BookDetailActivity.class);
+                    System.out.println("Inside bind-----------------"+mBook.getBookId());
+                    intent.putExtra("bookId",mBook.getBookId());
+                    ctx.startActivity(intent);
+
+
+                }
+            });
+            //delete= itemView.findViewById(R.id.deleteBook);
+            //update=itemView.findViewById(R.id.updateSeatchBook);
+
+        }
+
+        public void bind(Book book){
+
+            mBook = book;
+            title.setText(book.getTitle());
+            author.setText(book.getAuthor());
+            yearOfPub.setText(String.valueOf(book.getYearOfPub()));
+
+//            delete.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+//                    DbOperations.deleteBook(mBook.getBookId());
+//                    mBookList.remove(mBook);
+//                    System.out.println("Inside delete button called.  Book Id="+mBook.getTitle());
+//                    notifyDataSetChanged();
+//                }
+//            });
+
+//            update.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+//
+//                    Intent intent = new Intent(ctx, LibrarianUpdateBook.class);
+//                    intent.putExtra("bookObj",mBook);
+//                    ctx.startActivity(intent);
+//
+//                }
+//            });
+        }
+    }
+}
